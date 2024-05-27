3c7ede7e9ba89900d8d6801c132ad7c1f7af4de1
Geanyl17 <134925077+Geanyl17@users.noreply.github.com>
2024-05-27 14:28:33 +0800

    Alpha 0.1
    Camera Movement and more

--- Heat N Run/Assets/Gameplay/PlayerCombat.cs
+++ Heat N Run/Assets/Gameplay/PlayerCombat.cs
@@ -4,20 +4,42 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    
     public Animator animator;
-
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public float attackRate = 2f;
+    float nextAttackTime = 0f;
+    public LayerMask enemyLayers;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(Time.time >= nextAttackTime){
+             if (Input.GetMouseButtonDown(0))
         {
             Attack();
+            nextAttackTime = Time.time + 1f / attackRate;
         }
+     }
     }
+       
 
     void Attack()
         {
             animator.SetTrigger("Attack");
+
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+            foreach(Collider2D enemy in hitEnemies)
+            {
+                enemy.GetComponent<Enemy>().TakeDamage(30);
+            }
         }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+       
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
 }
--- Heat N Run/Assets/Gameplay/PlayerMovement.cs
+++ Heat N Run/Assets/Gameplay/PlayerMovement.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public Animator animator;
 
     public float runSpeed = 40f;
-
     float horizontalMove = 0f;
+    bool jump = false;
+
     void Update()
     {
        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+       animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+       if (Input.GetButtonDown("Jump"))
+       {
+            jump = true;
+       }
     }
 
-    void FixedUpdate()
+    void FixedUpdate ()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime,false, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
+
 }
 
