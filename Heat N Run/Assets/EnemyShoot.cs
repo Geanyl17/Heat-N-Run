27803b1efcf519696e7e75f4db8a9f13ceffa4b1
Geanyl17 <134925077+Geanyl17@users.noreply.github.com>
2024-05-30 05:45:44 +0800

    Hard Version
    Harder End Game Boss

--- Heat N Run/Assets/EnemyShoot.cs
+++ Heat N Run/Assets/EnemyShoot.cs
@@ -7,35 +7,47 @@ public class EnemyShoot : MonoBehaviour
     public GameObject bullet;
     public Transform bulletPos;
     public Animator animator;
-    private float timer;
+    private float m_FireTime = 2f;
+    public Vector2 m_Offset;
+    
+    private float timer = 0;
+    private float nextInterval;
+    public float minTime = 0.0f; // Minimum interval time
+    public float maxTime = 5.0f; // Maximum interval time
     // Start is called before the first frame update
-    void Start()
+  void Start()
     {
-
-        
+        // Set the initial random interval
+        SetNextInterval();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         timer += Time.deltaTime;
 
-        if(timer > 2)
+        if (timer > nextInterval)
         {
             timer = 0;
             shoot();
+            // Set the next random interval
+            SetNextInterval();
         }
-
-
     }
 
     void shoot()
     {
         animator.SetTrigger("Attack");
         Instantiate(bullet, bulletPos.position, Quaternion.identity);
-    
-    
+        Debug.Log("Shoot!");
     }
+
+    void SetNextInterval()
+    {
+        nextInterval = Random.Range(minTime, maxTime);
+        Debug.Log("Next interval: " + nextInterval);
+    }
+
+
 
 
 
